9a5f016163fb8b5669ac1877375487017d4080f1
ict009 <ict009@ict>
2015-01-27 22:18:23 +0700

    DMHT

--- jub_DMHT.sql
+++ jub_DMHT.sql
@@ -20,16 +20,3 @@ GROUP BY p.PID
  #end----------------------------------------------------------------------------------
 
 
-#จำนวนผู้ป่วยความดันโลหิตสูงผู้ป่วยใน อายุระหว่าง 15-35 ปี
-SET @hospcode='10725';
-SET @diagcode='I10';
-
-SELECT  p.PID,CONCAT(p.NAME,' ',p.LNAME) AS tname,p.CID,diagi.DIAGCODE,(YEAR(CURDATE())- YEAR(BIRTH) ) AS tage
-FROM f43_person p 
-LEFT JOIN f43_diagnosis_ipd diagi ON diagi.PID=p.PID
-WHERE p.hospcode=@hospcode
-    AND (YEAR(CURDATE())- YEAR(BIRTH))>14 AND (YEAR(CURDATE())- YEAR(BIRTH))<35
-    AND diagi.DIAGCODE=@diagcode
-GROUP BY p.PID
-
-#end----------------------------------------------------------------------------------
