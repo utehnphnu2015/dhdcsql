70edde08ff6d1ca1d96065a48f8c72502031dbae
Administrator <Administrator@192.168.187.102>
2015-02-07 00:29:43 +0700

    รายงายประชากรในเขตรับผิดชอบแยกตามกลุ่มสิทธิการรักษา

---
+++ knott/knott_person_right.sql
@@ -0,0 +1,8 @@
+set @pcu = '11255';
+
+SELECT concat(ins.instypecode,' : ',ins.instypename) as 'สิทธิการรักษา',count(p.PID) as 'คน'
+FROM person p,card c,cinstype_new ins
+where (p.HOSPCODE=c.HOSPCODE and p.PID=c.PID) and c.INSTYPE_NEW=ins.instypecode 
+and p.TYPEAREA in ('1','3') and p.DISCHARGE=9 and p.HOSPCODE=@pcu
+GROUP BY c.INSTYPE_NEW
+ORDER BY ins.instypecode
