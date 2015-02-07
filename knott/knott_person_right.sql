5777670c592af55f5b7a0e7778c12367ca9a25a1
Administrator <Administrator@192.168.187.167>
2015-02-07 15:28:32 +0700

    รายงานประชากรในเขตรับผิดชอบแยกตามกลุ่มสิทธิการรักษา

--- knott/knott_person_right.sql
+++ knott/knott_person_right.sql
@@ -5,4 +5,8 @@ FROM person p,card c,cinstype_new ins
 where (p.HOSPCODE=c.HOSPCODE and p.PID=c.PID) and c.INSTYPE_NEW=ins.instypecode 
 and p.TYPEAREA in ('1','3') and p.DISCHARGE=9 and p.HOSPCODE=@pcu
 GROUP BY c.INSTYPE_NEW
-ORDER BY ins.instypecode
+UNION
+SELECT 'รวม' as 'สิทธิการรักษา',count(p.PID) as 'คน'
+FROM person p,card c,cinstype_new ins
+where (p.HOSPCODE=c.HOSPCODE and p.PID=c.PID) and c.INSTYPE_NEW=ins.instypecode 
+and p.TYPEAREA in ('1','3') and p.DISCHARGE=9 and p.HOSPCODE=@pcu
