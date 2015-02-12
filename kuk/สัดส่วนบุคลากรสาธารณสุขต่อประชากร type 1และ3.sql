c3aa304a599824ae9a5149980dc09eb773b13d38
admin <admin@PC-Server>
2015-02-12 11:58:06 +0700

    kukks205 test

--- kuk/สัดส่วนบุคลากรสาธารณสุขต่อประชากร type 1และ3.sql
+++ kuk/สัดส่วนบุคลากรสาธารณสุขต่อประชากร type 1และ3.sql
@@ -1,3 +1,6 @@
+//kukks
+
+
 select care_team.hoscode,care_team.hosname,care_team.pop,care_team.doctor,concat('1 : ',round((care_team.pop/care_team.doctor),0)) as raio from
 (select h.hoscode,h.hosname,
 (select count(distinct CID) 
@@ -12,3 +15,4 @@ where pv.CID is not null and pv.PROVIDERTYPE in ('03','04','05','06')
 and pv.OUTDATE is null and pv.MOVETO is NULL and pv.HOSPCODE=h.hoscode) as doctor
 from chospital h 
 where h.provcode='41' and h.distcode='06' and h.hostype ='03') as care_team
+
